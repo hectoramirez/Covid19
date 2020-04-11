2c336262539e9bb8d76a17a3d4160565b3056c7f
hramirez <hector.ramirez@uv.es>
2020-04-11 23:38:16 +0200

    Update git_push.py

--- Automated/git_push.py
+++ Automated/git_push.py
@@ -1,7 +1,7 @@
 from git import Repo
 import os
 
-os.chdir('/Users/hramirez/GitHub/hectoramirez.github.io/covid')
+os.chdir('/Users/hramirez/GitHub/hectoramirez.github.io/covid ')
 
 PATH_OF_GIT_REPO = r'/Users/hramirez/GitHub/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'Daily update'
