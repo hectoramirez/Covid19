ecb5276f2af04374a57545cc231413ac7e7f3104
hramirez <vanhramirez@gmail.com>
2020-05-13 19:18:13 +0200

    Minor update

--- Automated/covid_func.py
+++ Automated/covid_func.py
@@ -313,7 +313,7 @@ def CovidPlots():
 
     # Remember: rolling() throws a list of dataframes where {'confirmed': 0, 'deaths': 1, 'confirmed':2}
 
-    yticks = [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 30000]
+    yticks = [2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 30000]
     bokeh_plot(rolling(n_since=30)[0], 'confirmed', n_since=30, tickers=yticks)
 
     yticks = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 3000]
