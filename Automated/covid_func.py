ad36a5d50e88443ad91f66be0a224ba6b9a982da
hramirez <vanhramirez@gmail.com>
2020-08-07 19:48:11 +0200

    Update covid_func.py

--- Automated/covid_func.py
+++ Automated/covid_func.py
@@ -21,7 +21,8 @@ def CovidPlots():
     }
     sns.set_style(custom_style)
 
-    os.chdir('/Users/hramirez/GitHub/Covid19/Automated')
+    #os.chdir('/Users/hramirez/GitHub/Covid19/Automated')
+    os.chdir('~/Covid19/Automated')
 
     # =========================================================================================  import
 
