6267ea5d7e4d599861f0a8a2ddbd939a2ef3a1e9
hramirez <vanhramirez@gmail.com>
2020-08-07 20:38:56 +0200

    Update git_push.py

--- Automated/git_push.py
+++ Automated/git_push.py
@@ -6,7 +6,7 @@ os.chdir('/home/ec2-user/hectoramirez.github.io/covid')
 
 #PATH_OF_GIT_REPO = r'/Users/hramirez/GitHub/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
 PATH_OF_GIT_REPO = r'/home/ec2-user/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'DailyUpdate'
+COMMIT_MESSAGE = 'U'
 
 
 def git_push():
