a1723c8bb135317e8bc17f220b88b17833160fa3
hramirez <vanhramirez@gmail.com>
2020-08-11 13:26:34 +0200

    Update git_push-EC2.py

--- Automated/git_push-EC2.py
+++ Automated/git_push-EC2.py
@@ -6,14 +6,12 @@ import os
 os.chdir('/home/ec2-user/hectoramirez.github.io/covid')
 
 PATH_OF_GIT_REPO = r'/home/ec2-user/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = " "
-
 
 def git_push():
 
     repo = Repo(PATH_OF_GIT_REPO)
     repo.git.add('--all')  # update=True)
-    repo.index.commit(COMMIT_MESSAGE)
+    repo.index.commit('daily')
     origin = repo.remote(name='origin')
     origin.push()
 
