3e8a7f71207b77ee567ca7581bf9dd10a4f528a3
hramirez <vanhramirez@gmail.com>
2020-08-11 12:41:54 +0200

    EC2 -u

--- Automated/git_push-EC2.py
+++ Automated/git_push-EC2.py
@@ -5,12 +5,10 @@ import sys
 #reload(sys)  
 #sys.setdefaultencoding('utf8')
 
-os.chdir('/Users/hramirez/GitHub/hectoramirez.github.io/covid')
-#os.chdir('/home/ec2-user/hectoramirez.github.io/covid')
+os.chdir('/home/ec2-user/hectoramirez.github.io/covid')
 
-#PATH_OF_GIT_REPO = r'/Users/hramirez/GitHub/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
 PATH_OF_GIT_REPO = r'/home/ec2-user/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = str('daily update')
+COMMIT_MESSAGE = 'daily update'
 
 
 def git_push():
