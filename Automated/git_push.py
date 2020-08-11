9aa139d2e6a44cb7cc98034bc4bd45cdb99990e4
hramirez <vanhramirez@gmail.com>
2020-08-11 15:06:52 +0200

    Create git_push.py

---
+++ Automated/git_push.py
@@ -0,0 +1,19 @@
+from git import Repo
+import os
+
+os.chdir('/Users/hramirez/GitHub/hectoramirez.github.io/covid')
+
+PATH_OF_GIT_REPO = r'/Users/hramirez/GitHub/hectoramirez.github.io/.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'daily update'
+
+
+def git_push():
+
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add('--all')  # update=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+git_push()
