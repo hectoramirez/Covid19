fee7f9cbe47c20f9950ad72e970b8777fb6b70b4
hramirez <vanhramirez@gmail.com>
2020-08-11 14:01:23 +0200

    Create git_push_EC2.py

---
+++ Automated/git_push_EC2.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+from git import Repo
+import os
+
+os.chdir('/home/ec2-user/hectoramirez.github.io/covid')
+
+PATH_OF_GIT_REPO = r'/home/ec2-user/hectoramirez.github.io/.git'
+
+def git_push():
+
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add('--all')  # update=True)
+    repo.index.commit('daily')
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+git_push()
